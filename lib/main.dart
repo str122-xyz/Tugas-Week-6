6fdda1614c00aac6595854c59441b8ecedd833b2
Satriahrl_ <satriaherlambang22@gmail.com>
2025-11-03 14:41:11 +0700

    create main page and theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,21 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'NIM : 1123150070, Satria Herlambang',
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigo),
+      ),
+    );
+  }
+}
